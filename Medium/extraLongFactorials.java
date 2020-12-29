6088fa4c7546bf73c0e6763729ad60f483adb0bb
Muhammad Shiddiq Fathullah <73525864+mas-diq@users.noreply.github.com>
2020-12-29 23:58:48 +0700

    Add files via upload

--- Medium/extraLongFactorials.java
+++ Medium/extraLongFactorials.java
@@ -1,3 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
 public class extraLongFactorials {
-    
+    static BigInteger kali(int n) {
+        BigInteger f = new BigInteger("1");
+
+        for (int i = n; i > 0; i--) {
+            f = f.multiply(BigInteger.valueOf(i));
+        }
+        return f;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(kali(n));
+    }
 }
